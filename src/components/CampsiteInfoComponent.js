c03ad5068c73a0265b49bc844ce555322afb73a0
Ryan Eldon <47878230+rdogstyle@users.noreply.github.com>
2020-11-30 16:24:16 -0500

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class CampsiteInfo extends Component {
 
@@ -32,26 +32,24 @@ class CampsiteInfo extends Component {
                 </div>
             )
         }
-        return (<div></div>)
+        return <div />
     }
 
     render() {
 
         if (this.props.campsite) {
             return (
-                (<div className='row'>
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
-                </div>)
-            )
-        } else {
-            return (
-                <div>
-
+                <div className='container'>
+                    <div className='row'>
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
             );
         }
+        return <div />
     }
 }
+
 
 export default CampsiteInfo;
